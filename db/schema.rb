fb8c505ba628b621583e10fb613cdff16bd88a03
sangey <pragmatic.box@gmail.com>
2014-03-15 22:37:00 -0400

    added images

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140312000419) do
+ActiveRecord::Schema.define(version: 20140315232132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: true do |t|
+  create_table "users", force: true do |t|
     t.string   "name"
-    t.text     "content"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
